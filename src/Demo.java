47b7d6802b963369fae48f4520bdedd8634bc21d
kismatpan <kismatpanchbhai720@gmail.com>
2024-10-02 23:29:23 +0530

    first commit

---
+++ src/Demo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Demo {
+
+	public static void main(String[] args)
+	{    
+		Scanner scan=new Scanner(System.in);
+		System.out.println("hey user please enter Your name");
+		String Name=scan.next();
+		System.out.println("Now,Tell me your Surname");
+		String Surname=scan.next();
+		System.out.println("Here are the Yooour details " +Name + " " + Surname);
+
+	}
+
+}
